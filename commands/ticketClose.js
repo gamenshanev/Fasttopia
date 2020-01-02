40b54202de0986a37a028bec9d7443f44952e367
gamenshanev <49565972+gamenshanev@users.noreply.github.com>
2020-01-02 14:06:46 +0100

    Update ticketClose.js

--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // Id van category van tickets.
-    const categoryId = "661855544000380928";
+    const categoryId = "662276542885527562";
 
     // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
     if (message.channel.parentID == categoryId) {
